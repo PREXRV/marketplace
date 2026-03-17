5b3348d9dca7b7ebed914a8d4dfce1d8d2601052
PREXRV <xpestigeox@gmail.com>
2026-03-17 07:32:31 +0500

    Backend deployed

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api';
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_URL = 'https://marketplace-backend-70h9.onrender.com/api';
+const API_BASE_URL = 'https://marketplace-backend-70h9.onrender.com/api';
 
 
 
